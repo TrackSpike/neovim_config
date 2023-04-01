e28cd1852ca1748a4f9c5bcf921b07d23f71d088
Bryce Cole <brycewcole@gmail.com>
2023-04-01 11:43:33 -0400

    Adding inital customizations

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,24 @@
 return {
+  {
+    "kylechui/nvim-surround",
+    lazy = false,
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  {
+    "sunjon/shade.nvim",
+    lazy = false,
+    config = function()
+      require("shade").setup {
+        overlay_opacity = 70,
+        opacity_step = 1,
+        keys = {},
+      }
+    end,
+  },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
