de08e68e5fc611894c204953cdff50e659317d6f
Bryce Cole <brycewcole@gmail.com>
2023-04-03 08:32:53 -0400

    Adding copilot and prettier. Removed shade.

--- plugins/user.lua
+++ plugins/user.lua
@@ -8,17 +8,17 @@ return {
       }
     end,
   },
-  {
-    "sunjon/shade.nvim",
-    lazy = false,
-    config = function()
-      require("shade").setup {
-        overlay_opacity = 70,
-        opacity_step = 1,
-        keys = {},
-      }
-    end,
-  },
+  -- {
+  --   "sunjon/shade.nvim",
+  --   lazy = false,
+  --   config = function()
+  --     require("shade").setup {
+  --       overlay_opacity = 70,
+  --       opacity_step = 1,
+  --       keys = {},
+  --     }
+  --   end,
+  -- },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
